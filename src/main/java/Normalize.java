a9e4d3b123a76a15c8b62a4975ed42d2112d0724
zhangshiqiu <zhangshiqiu1993@126.com>
2017-02-26 11:57:53 -0800

    modified normalize class

--- src/main/java/Normalize.java
+++ src/main/java/Normalize.java
@@ -10,58 +10,61 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
-/**
- * Created by zhangshiqiu on 2017/2/16.
- */
-
 public class Normalize {
+
     public static class NormalizeMapper extends Mapper<LongWritable, Text, Text, Text> {
+
+        // map method
         @Override
-        public void map(LongWritable key, Text value, Context context)
-                throws IOException, InterruptedException {
-            // value: movieA : movieB \t relation
-            String[] movie_realtion = value.toString().trim().split("\t");
-            if (movie_realtion.length != 2){
-                return;
-            }
-            String[] movies = movie_realtion[0].split(":");
-            String movieA = movies[0], movieB = movies[1];
-            context.write(new Text(movieA), new Text(movieB + "=" + movie_realtion[1]));
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+            //movieA:movieB \t relation
+            //collect the relationship list for movieA
+            String[] movie_relation = value.toString().trim().split("\t");
+            String[] movies = movie_relation[0].split(":");
+            StringBuilder builder = new StringBuilder();
+            builder.append(movies[1]).append(":").append(movie_relation[1]);
+            context.write(new Text(movies[0]), new Text(builder.toString()));
         }
     }
 
     public static class NormalizeReducer extends Reducer<Text, Text, Text, Text> {
+        // reduce method
         @Override
         public void reduce(Text key, Iterable<Text> values, Context context)
                 throws IOException, InterruptedException {
-            // sum --> denominator
-            // context write transpose
-            // key = movieA value = {movie2=10, movie3=30, movie4=15}
 
+            //key = movieA, value=<movieB:relation, movieC:relation...>
+            //normalize each unit of co-occurrence matrix
+            // outputKey : movieB
+            // outputValue : movieA=(normalized value)
             int sum = 0;
-            Map<String, Integer> map = new HashMap<>();
+            String movieA = key.toString().trim();
+            Map<String, Integer> map = new HashMap<String, Integer>();
             for (Text value : values) {
-                String[] movie_relation = value.toString().split("=");
-                int relation = Integer.parseInt(movie_relation[1].trim());
+                String[] movie_rating = value.toString().trim().split(":");
+                int relation = Integer.parseInt(movie_rating[1]);
                 sum += relation;
-                map.put(movie_relation[0], relation);
+                map.put(movie_rating[0], relation);
             }
 
+            StringBuilder builder = new StringBuilder();
             for (Map.Entry<String, Integer> entry : map.entrySet()){
                 String outputKey = entry.getKey();
-                String outputValue = key.toString() + "=" + ((double) entry.getValue() / sum);
-                context.write(new Text(outputKey), new Text(outputValue));
+                builder.append(movieA).append("=").append((double)entry.getValue()/sum);
+                context.write(new Text(outputKey), new Text(builder.toString()));
+                builder.setLength(0);
             }
         }
     }
 
-    public static void main(String[] args) throws Exception{
-        Configuration configuration = new Configuration();
-        Job job = Job.getInstance(configuration);
+    public static void main(String[] args) throws Exception {
 
+        Configuration conf = new Configuration();
+
+        Job job = Job.getInstance(conf);
         job.setMapperClass(NormalizeMapper.class);
         job.setReducerClass(NormalizeReducer.class);
 
